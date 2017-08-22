4fccff4481320695ceea17d3102e1861ef9ba7d5
Greg Spiridonov <gspiridonov@gmail.com>
2017-08-22 01:22:23 -0400

    Objs

---
+++ Assets/Scripts/Behaviour/CheckHighest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckHighest : Sequence
+{
+    string name;
+    GameObject agent;
+    public CheckHighest(GameObject agent, string name)
+    {
+        this.name = name;
+        this.agent = agent;
+    }
+
+    public override bool Resolve()
+    {
+        AIComponent aic = agent.GetComponent<AIComponent>();
+
+        float important = aic.wants[name];
+
+        foreach (float val in aic.wants.Values)
+        {
+            if (important < val)
+            {
+                return true;
+            }
+        }
+
+        for (int i = 0; i < children.Count; i++)
+        {
+            if (!children[i].Resolve())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
